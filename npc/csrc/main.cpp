3d09611428c1a464cdda3d220af6b17784179541
tracer-ysyx <tracer@ysyx.org>
2025-04-17 15:53:31 +0800

    > sim RTL ysyx_22040000 daiman Linux daiman 6.8.0-57-generic #59~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 19 17:07:41 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:53:31 up  4:55,  1 user,  load average: 0.10, 0.14, 0.09

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,6 +1,17 @@
-#include <stdio.h>
+#include <nvboard.h>
+#include <Vtop.h>
+
+static TOP_NAME dut;
+
+void nvboard_bind_all_pins(TOP_NAME* top);
+
 
 int main() {
-  printf("Hello, ysyx!\n");
-  return 0;
+  nvboard_bind_all_pins(&dut);
+  nvboard_init();
+
+  while(1) {
+    nvboard_update();
+    dut.eval();
+  }
 }
