faded386864b37bce11ca89e2e2b5f2cf6cc9a3f
tracer-ysyx <tracer@ysyx.org>
2025-04-24 14:29:52 +0800

    > sim RTL ysyx_22040000 daiman Linux daiman 6.8.0-57-generic #59~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 19 17:07:41 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:29:52 up  1:58,  1 user,  load average: 0.62, 0.21, 0.06

--- npc/vsrc/mux41.v
+++ npc/vsrc/mux41.v
@@ -55,10 +55,10 @@ module mux41(input [1:0] Y,
 input [1:0] X0,X1,X2,X3,
 output F);
 
-MuxKeyWithDefault u_MuxKeyWithDefault #(4,2,1) mux_2bits(
+MuxKeyWithDefault #(4,2,1) mux_2bits(
     .out         	(F          ),
     .key         	(Y          ),
     .default_out 	(1'b0  ),
     .lut         	({2'b00,X[0],2'b01,X[1],2'b10,X[2],2'b11,X[3]})
 );
-endmodue
+endmodule
