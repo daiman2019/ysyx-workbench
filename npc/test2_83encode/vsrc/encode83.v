8562702794b7eeefeea5e958b50a406a90e5fcd2
tracer-ysyx <tracer@ysyx.org>
2025-04-25 09:56:22 +0800

    > sim RTL ysyx_22040000 daiman Linux daiman 6.8.0-57-generic #59~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 19 17:07:41 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:56:22 up 37 min,  1 user,  load average: 0.49, 0.20, 0.12

--- npc/test2_83encode/vsrc/encode83.v
+++ npc/test2_83encode/vsrc/encode83.v
@@ -5,7 +5,7 @@ output input_flag);
 assign input_flag = ~(x==8'b0);
 always@(*) begin
     if(ena) begin
-    casez(x)
+        casez(x)
         8'b1zzzzzzz:y=3'd7;
         8'b01zzzzzz:y=3'd6;
         8'b001zzzzz:y=3'd5;
@@ -14,8 +14,8 @@ always@(*) begin
         8'b000001zz:y=3'd2;
         8'b0000001z:y=3'd1;
         8'b00000001:y=3'd0;
-        default:y=3'bxxx;
-    endcasez
+        default:y=3'b000;
+        endcasez
     end
     else begin
         y=3'b000;
