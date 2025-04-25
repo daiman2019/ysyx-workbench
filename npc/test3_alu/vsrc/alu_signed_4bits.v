bc99ffdab536cbf7d3a42ecf49f03e226c9b943e
tracer-ysyx <tracer@ysyx.org>
2025-04-25 17:12:49 +0800

    > sim RTL ysyx_22040000 daiman Linux daiman 6.8.0-57-generic #59~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 19 17:07:41 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:12:49 up  5:36,  1 user,  load average: 0.23, 0.38, 0.59

--- npc/test3_alu/vsrc/alu_signed_4bits.v
+++ npc/test3_alu/vsrc/alu_signed_4bits.v
@@ -17,41 +17,35 @@ module alu_signed_4bits(
                 {carry_out, result} = A + B;
                 overflow = (A[3] == B[3]) && (result[3] != A[3]);
                 zero_flag = ~(|result);
-                equal_flag = 0;less_flag = 0;
             end
             3'b001: begin // SUB
                 temp = ({4{1'b1}}^B); 
                 {carry_out, result} = A + temp + 1'b1;
                 overflow = (A[3] == temp[3]) && (result[3] != A[3]);
                 zero_flag = ~(|result);
-                equal_flag = 0;less_flag = 0;
             end
             3'b010: begin // NOT A
                 result = ~A;
                 zero_flag = ~(|result);
-                carry_out = 0;overflow = 0;equal_flag = 0;less_flag = 0;
             end
             3'b011: begin // A AND B
                 result = A & B;
                 zero_flag = ~(|result);
-                carry_out = 0;overflow = 0;equal_flag = 0;less_flag = 0;
             end
             3'b100: begin // A or B
                 result = A | B;
                 zero_flag = ~(|result);
-                carry_out = 0;overflow = 0;equal_flag = 0;less_flag = 0;
             end
             3'b101: begin // A XOR B
                 result = A ^ B;
                 zero_flag = ~(|result);
-                carry_out = 0;overflow = 0;equal_flag = 0;less_flag = 0;
             end
             3'b110:begin //A<B
                 temp = ({4{1'b1}}^B)+1'b1; //B -> -B
                 {carry_out, result} = A + temp;
                 overflow = (A[3] == temp[3]) && (result[3] != A[3]);
                 less_flag = overflow ^ result[3];
-                zero_flag = ~(|result);equal_flag = 0;
+                zero_flag = ~(|result);
             end
             3'b111:begin //A==B
                 temp = ({4{1'b1}}^B)+1'b1; //B -> -B
