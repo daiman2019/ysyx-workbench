3ba46eccdbb1a6d6d3739b9b73324895d5bf413e
tracer-ysyx <tracer@ysyx.org>
2025-04-25 09:54:15 +0800

    > sim RTL ysyx_22040000 daiman Linux daiman 6.8.0-57-generic #59~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 19 17:07:41 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:54:15 up 35 min,  1 user,  load average: 0.06, 0.08, 0.08

---
+++ npc/test2_83encode/csrc/main.cpp
@@ -0,0 +1,86 @@
+#include <verilated.h>
+#include <nvboard.h>
+#include <Vencode83.h>
+#include <verilated_vcd_c.h> //vcd waveform trace
+
+VerilatedContext* contextp=NULL;
+VerilatedVcdC* tfp = NULL;
+static Vencode83* top;
+static TOP_NAME encode83;
+void nvboard_bind_all_pins(TOP_NAME* name);
+
+void step_and_dump_wave()
+{
+    top->eval();
+    contextp->timeInc(1);
+    tfp->dump(contextp->time());
+}
+
+void sim_init()
+{
+    contextp = new VerilatedContext;
+    tfp = new VerilatedVcdC;
+    top = new Vencode83;
+    contextp->traceEverOn(true);
+    top->trace(tfp,0);
+    tfp->open("mux41_waveform.vcd");//.vcd
+}
+
+void sim_exit()
+{
+    step_and_dump_wave();
+    tfp->close();
+}
+void sim_run()
+{
+    top->Y=0;top->X0=0;top->X1=0;top->X2=0;top->X3=0;step_and_dump_wave();
+    top->Y=0;top->X0=1;top->X1=0;top->X2=0;top->X3=0;step_and_dump_wave();
+    top->Y=0;top->X0=0;top->X1=1;top->X2=0;top->X3=0;step_and_dump_wave();
+    top->Y=0;top->X0=0;top->X1=0;top->X2=1;top->X3=0;step_and_dump_wave();
+    top->Y=0;top->X0=0;top->X1=0;top->X2=0;top->X3=1;step_and_dump_wave();
+    
+    top->Y=1;top->X0=0;top->X1=0;top->X2=0;top->X3=0;step_and_dump_wave();
+    top->Y=1;top->X0=0;top->X1=0;top->X2=0;top->X3=0;step_and_dump_wave();
+    top->Y=1;top->X0=0;top->X1=1;top->X2=0;top->X3=0;step_and_dump_wave();
+    top->Y=1;top->X0=0;top->X1=0;top->X2=1;top->X3=0;step_and_dump_wave();
+    top->Y=1;top->X0=0;top->X1=0;top->X2=0;top->X3=1;step_and_dump_wave();
+    
+    top->Y=2;top->X0=0;top->X1=0;top->X2=0;top->X3=0;step_and_dump_wave();
+    top->Y=2;top->X0=1;top->X1=0;top->X2=0;top->X3=0;step_and_dump_wave();
+    top->Y=2;top->X0=0;top->X1=1;top->X2=0;top->X3=0;step_and_dump_wave();
+    top->Y=2;top->X0=0;top->X1=0;top->X2=1;top->X3=0;step_and_dump_wave();
+    top->Y=2;top->X0=0;top->X1=0;top->X2=0;top->X3=1;step_and_dump_wave();
+    
+    top->Y=3;top->X0=0;top->X1=0;top->X2=0;top->X3=0;step_and_dump_wave();
+    top->Y=3;top->X0=1;top->X1=0;top->X2=0;top->X3=0;step_and_dump_wave();
+    top->Y=3;top->X0=0;top->X1=1;top->X2=0;top->X3=0;step_and_dump_wave();
+    top->Y=3;top->X0=0;top->X1=0;top->X2=1;top->X3=0;step_and_dump_wave();
+    top->Y=3;top->X0=0;top->X1=0;top->X2=0;top->X3=1;step_and_dump_wave();
+    
+    top->Y=4;top->X0=0;top->X1=0;top->X2=0;top->X3=0;step_and_dump_wave();
+    top->Y=4;top->X0=1;top->X1=0;top->X2=0;top->X3=0;step_and_dump_wave();
+    top->Y=4;top->X0=0;top->X1=1;top->X2=0;top->X3=0;step_and_dump_wave();
+    top->Y=4;top->X0=0;top->X1=0;top->X2=1;top->X3=0;step_and_dump_wave();
+    top->Y=4;top->X0=0;top->X1=0;top->X2=0;top->X3=1;step_and_dump_wave();
+}
+
+/*int main() //for verilator to check vcd waveform
+{
+    sim_init();
+    sim_run();
+    sim_exit();
+    return 0;
+}*/
+
+int main() // for nvboard test
+{
+  nvboard_bind_all_pins(&encode83);
+  nvboard_init();
+  
+  while(1) {
+    nvboard_update();
+    encode83.eval();
+  }
+  nvboard_quit();
+  return 0;
+}
---
+++ npc/test2_83encode/vsrc/encode83.v
@@ -0,0 +1,32 @@
+module encode83(input [7:0] x,
+input ena,
+output [2:0] y,
+output input_flag);
+assign input_flag = ~(x==8'b0);
+always@(*) begin
+    if(ena) begin
+    casez(x)
+        8'b1zzzzzzz:y=3'd7;
+        8'b01zzzzzz:y=3'd6;
+        8'b001zzzzz:y=3'd5;
+        8'b0001zzzz:y=3'd4;
+        8'b00001zzz:y=3'd3;
+        8'b000001zz:y=3'd2;
+        8'b0000001z:y=3'd1;
+        8'b00000001:y=3'd0;
+        default:y=3'bxxx;
+    encase
+    end
+    else begin
+        y=3'b000;
+    end 
+end
+endmodule
+
+
+
+
+
+
+
+
