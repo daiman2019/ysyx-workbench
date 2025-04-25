ce1aad7eda091f025cb454a2768ec7a3a511eaa8
tracer-ysyx <tracer@ysyx.org>
2025-04-25 10:32:11 +0800

    > sim RTL ysyx_22040000 daiman Linux daiman 6.8.0-57-generic #59~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 19 17:07:41 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:32:11 up  1:13,  1 user,  load average: 0.47, 0.31, 0.23

--- npc/test2_83encode/vsrc/top.v
+++ npc/test2_83encode/vsrc/top.v
@@ -4,12 +4,12 @@ module top(input [7:0] x,
         output input_flag,
         output [6:0] hout);
  
-    encode83(.x(x),
+    encode83 encoder(.x(x),
      .ena(ena),
      .y(y),
      .input_flag(input_flag)); // instantiation of encode83 module
 
-    data2seg(.data({1'b0,y}),.neg_show(1'b1),.hout(hout));
+    data2seg decoder(.data({1'b0,y}),.neg_show(1'b1),.hout(hout));
 
 
 endmodule
