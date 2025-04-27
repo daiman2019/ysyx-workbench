660ea55631a224c981c7d7ef20042f28d7d69c28
tracer-ysyx <tracer@ysyx.org>
2025-04-27 10:03:32 +0800

    > sim RTL ysyx_22040000 daiman Linux daiman 6.8.0-57-generic #59~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 19 17:07:41 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:03:32 up 59 min,  1 user,  load average: 0.08, 0.10, 0.09

--- npc/test6_lfsr/vsrc/top.v
+++ npc/test6_lfsr/vsrc/top.v
@@ -2,7 +2,7 @@ module top(
     input clk,
     input rst,
     input [7:0] init,
-    output [6:0] hout
+    output [13:0] hout
 );
     wire [7:0] lfsr_out;
     lfsr_8bits lfsr_test(
@@ -11,8 +11,12 @@ module top(
     .init(init),
     .lfsr_out(lfsr_out));
 
-    data2seg show(.data(lfsr_out),
+    data2seg show_low(.data(lfsr_out[3:0]),
     .neg_show(1'b1),
-    .hout(hout));
+    .hout(hout[6:0]));
+
+    data2seg show_high(.data(lfsr_out[7:4]),
+    .neg_show(1'b1),
+    .hout(hout[13:7]));
 
 endmodule
