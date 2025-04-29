697e83df170f0d8f03e9b7648d92a5a7469a5a8c
tracer-ysyx <tracer@ysyx.org>
2025-04-29 17:16:52 +0800

    > compile NEMU ysyx_22040000 daiman Linux daiman 6.8.0-57-generic #59~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 19 17:07:41 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:16:52 up 1 day,  8:24,  1 user,  load average: 0.12, 0.32, 0.29

--- nemu/src/utils/state.c
+++ nemu/src/utils/state.c
@@ -18,9 +18,9 @@
 NEMUState nemu_state = { .state = NEMU_STOP };
 
 int is_exit_status_bad() {
-  int good = (nemu_state.state == NEMU_ABORT && nemu_state.halt_ret == 1) ||
+  int good = (nemu_state.state == NEMU_ABORT && nemu_state.halt_ret == 0) ||
     (nemu_state.state == NEMU_QUIT);
   printf("nemu_state.state = %d, nemu_state.halt_ret = %d\n", nemu_state.state, nemu_state.halt_ret);
   printf("is_exit_status_bad: %s\n", good ? "true" : "false");
-  return !good;
+  return good;
 }
