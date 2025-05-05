8d14ac732a3aaf5dba7c032c49def324ec12bc76
tracer-ysyx <tracer@ysyx.org>
2025-05-05 16:41:47 +0800

    > sim RTL ysyx_22040000 daiman Linux daiman 6.8.0-57-generic #59~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 19 17:07:41 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:41:47 up 35 min,  1 user,  load average: 0.56, 0.54, 0.65

--- npc/test7_fsm_and_keyboard/vsrc/key_identify.v
+++ npc/test7_fsm_and_keyboard/vsrc/key_identify.v
@@ -1,6 +1,20 @@
-module key_identify(
-    input clk,
-    input resetn,
-    input [7:0] data,
-    output reg
-)
+module key_counter(
+        input clk,
+        input resetn,
+        input[7:0] data,
+        output reg[7:0] counter
+    );
+
+always@(posedge clk)begin
+    if(resetn==1'b0)begin
+        counter<=8'b0;
+    end
+    else begin
+        if(data==8'hf0)begin
+            counter<=counter+1'b1;
+        end
+    end
+end
+
+
+endmodule
--- npc/test7_fsm_and_keyboard/vsrc/top.v
+++ npc/test7_fsm_and_keyboard/vsrc/top.v
@@ -8,18 +8,27 @@ module top(
 );
     wire [7:0] counter;
     wire [7:0] ascii;
+    wire [7:0] data;
     //接受键盘送来的数据
     ps2_keyboard(clk,clrn,ps2_clk,ps2_data,
                     data,ready,nextdata_n,overflow);
-    
+    assign data = ready?data:8'b0;
+
     always@(posedge clk)begin
         if(ready&&~overflow)
             nextdata_n<=0;
         else
             nextdata_n<=1;
     end
-    //识别按键
 
+    //统计按键次数
+    key_counter(
+        .clk(clk),
+        .resetn(clrn),
+        .data(data),
+        .counter(counter)
+    );
+    
     //将扫描码转换为ASCII码
     scan_code2ascii(
     .data(data),
@@ -37,9 +46,11 @@ module top(
     data2seg show_third(.data(ascii[3:0]),
     .neg_show(1'b0),
     .hout(hout[20:14]));
+
     data2seg show_forth(.data(ascii[7:4]),
     .neg_show(1'b0),
     .hout(hout[27:21]));
+    
     //show key numbers
     data2seg show_fifth(.data(counter[3:0]),
     .neg_show(1'b0),
