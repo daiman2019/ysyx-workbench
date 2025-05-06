5fb97cd793f495e47c3954f50860fe932d505b51
tracer-ysyx <tracer@ysyx.org>
2025-05-06 11:23:34 +0800

    > sim RTL ysyx_22040000 daiman Linux daiman 6.8.0-57-generic #59~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 19 17:07:41 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:23:34 up  2:07,  1 user,  load average: 0.60, 0.31, 0.31

--- npc/test7_fsm_and_keyboard/vsrc/top.v
+++ npc/test7_fsm_and_keyboard/vsrc/top.v
@@ -76,7 +76,7 @@ module top (
         else
             state<=next;
     end
-    assign out_data = (~ready_before && ready) ? data : 0;
+    assign out_data = (state==show) ? data : 0;
     // always@(posedge clk)begin
     //     case (state)
     //         init:out_data<=0;
