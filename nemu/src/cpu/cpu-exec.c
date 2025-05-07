934d890d689b554a2d7c8cc990cb232f7c76cb1f
tracer-ysyx <tracer@ysyx.org>
2025-05-07 16:39:29 +0800

    > compile NEMU ysyx_22040000 daiman Linux daiman 6.8.0-57-generic #59~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 19 17:07:41 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:39:29 up  7:59,  1 user,  load average: 0.51, 0.37, 0.27

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -133,6 +133,6 @@ void cpu_exec(uint64_t n) {
           nemu_state.halt_pc);
       // fall through
     case NEMU_QUIT: statistic();
-    //case NEMU_STOP:sdb_mainloop();
+    case NEMU_STOP:sdb_mainloop();
   }
 }
