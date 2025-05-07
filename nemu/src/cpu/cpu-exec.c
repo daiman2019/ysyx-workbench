ded8b9d359d0ef15a81aab7b619a0cd051c0f113
tracer-ysyx <tracer@ysyx.org>
2025-05-07 17:07:55 +0800

    > compile NEMU ysyx_22040000 daiman Linux daiman 6.8.0-57-generic #59~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 19 17:07:41 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:07:55 up  8:27,  1 user,  load average: 0.02, 0.12, 0.19

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -124,7 +124,7 @@ void cpu_exec(uint64_t n) {
 
   switch (nemu_state.state) {
     case NEMU_RUNNING: nemu_state.state = NEMU_STOP; break;
-    case NEMU_STOP:break;
+    //case NEMU_STOP:break;
     case NEMU_END: case NEMU_ABORT:
       Log("nemu: %s at pc = " FMT_WORD,
           (nemu_state.state == NEMU_ABORT ? ANSI_FMT("ABORT", ANSI_FG_RED) :
